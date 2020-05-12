26f98ee64d5da97b3afe0cad30c9b87bb5ebf224
sabaikanwal <sabaiman941@gmail.com>
2020-05-12 16:30:57 +0500

    added my github name

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const  express = require('express');
 const app=express();
 const mongoose=require('mongoose');
+const bodyParser=require('body-parser');
+const cookieParser=require('cookie-parser');
+const config=require('./config/key');
+const { User}=require('./models/user');
+app.get('/', (req, res) => {
+   res.json({"hello ~1":"h1 ~~"});
+  
+});
 //const URI='mongodb+srv://sabai:database1122@cluster0-qw7s6.mongodb.net/test?retryWrites=true&w=majority';
 //const connectDB= async() =>{
   //await mongoose.connect(URI);
@@ -18,12 +26,28 @@ const mongoose=require('mongoose');
     //.then(console.log('db connected'))
     //.catch(err =>console.log(err));
 
-mongoose.connect('mongodb+srv://saba:123@cluster0-qw7s6.mongodb.net/test?retryWrites=true&w=majorit',
+mongoose.connect(config.mongoURI,
 {useNewUrlParser:true}).then(() =>console.log('DB CONNECTED'))
                   .catch(err =>console.error(err));
                 
 app.get('/', (req, res) => {
  res.send('hello world')
     
+});
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.post('/api/users/register', (req, res) => {
+  
+  const user=new User(req.body)
+  user.save((err, userData) =>{
+    if(err)
+    return res.json({success:false, err})
+
+  
+  return res.status(200).json({
+    success:true
+  })
+})
 });
 app.listen(3000);
